6f13b9ea432c6013cde482f99e0c89c8a8cf64c7
gboone <gregory.boone@cfpb.gov>
2014-05-22 18:30:02 -0400

    Some of this works, not all of it but a good start

--- taxonomy.php
+++ taxonomy.php
@@ -16,7 +16,13 @@ class Taxonomy_Command extends WP_CLI_Command {
     *
     *     wp taxonomy migrate tag group
     *
-    * @synopsis <from> <to> [--post_type=<foo>] [--exclude=<bar>] [--after=<date>] [--before=<date>] [--terms=<terms>]
+    * @synopsis <from> <to> --include=<bar> [--post_type=<foo>]
+    * @todo [--post_type=<foo>] [--exclude=<bar>] [--after=<date>] [--before=<date>] [--terms=<terms>]
+    *
+    **/
+    /**
+    * @subcommand migrate
+    * @alias m
     *
     **/
     function migrate( $args, $assoc_args ) {
@@ -24,26 +30,44 @@ class Taxonomy_Command extends WP_CLI_Command {
         $to = $args[1];
         extract( $assoc_args );
         $message = "Will migrate all $from to $to";
+        $args = array('posts_per_page' => -1);
+        if ( $include != 'all' ) {
+            $args['include'] = $include;
+        }
+        $posts = get_posts($args);
+
+        var_dump($posts);
+        foreach ( $posts as $p ) {
+            $terms = wp_get_post_terms( $p->ID, $from, array( 'fields' => 'slugs' ) );
+            $new = wp_set_object_terms( $p->ID, $terms, $to, true);
+        }
+        var_dump($terms);
+        var_dump($new);
         if ( isset( $post_type ) ) {
             $message .= " in the $post_type post type";
+            exit('Unimplemented' );
         }
         if ( isset( $exclude ) ) {
             $message .= " except for posts $exclude";
+            exit('Unimplemented' );
         }
         if ( isset( $after ) && isset( $before ) ) {
             $message .= " between $before and $after";
+            exit('Unimplemented' );
         } elseif ( isset( $after ) ) {
             $message .= " after $after";
+            exit('Unimplemented' );
         } elseif ( isset( $before ) ) {
             $message .= " before $before";
+            exit('Unimplemented' );
         }
         if ( isset($terms) ) {
             $message .= " against only these terms: $terms";
+            exit('Unimplemented' );
         }
 
         $message .= ".";
         WP_CLI::success( $message );
-        WP_CLI::success( "FYI: Nothing actually happened, this is just a prototype." );
     }
 }
 
